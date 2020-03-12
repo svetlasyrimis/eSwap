cef5df9606ec79572cc777e05ec9b0f06c1a1f5c
Dwayne <dmoneymunro@gmail.com>
2020-03-12 10:38:10 -0400

    Set up homepage

---
+++ client/src/components/AppContainer.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import Home from './Home';
+import { Route, Switch } from 'react-router-dom';
+// import { Route } from 'react-router-dom';
+
+export class AppContainer extends Component {
+    render() {
+        return (
+            <div>
+                <Switch>
+                    <Route exact path="/" component={Home} />
+                </Switch>
+            </div>
+        )
+    }
+}
+
+export default AppContainer;
---
+++ client/src/components/Home.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import { Jumbotron, Button } from 'react-bootstrap';
+
+const Home = () => {
+    return (
+        <div className="homepage">
+            <div className="top-img">
+                <img src="/img/top-banner.jpg" alt="" width="100%" height="400px" />
+            </div>
+            <Jumbotron className="home-container">
+                <div className="home-nav" style={{ display: "flex", justifyContent: "space-between" }}>
+                    <span>Sign Up</span>
+                    <span>Login</span>
+                </div>
+                <h1 className="home-name">Mando List</h1>
+                <input type="text" name="" id=""/>
+                <Button>Search</Button>
+            </Jumbotron>
+            <div className="bottom-img">
+                <img src="img/bottom-banner.jpg" alt="" width="100%" height="400px" />
+            </div>
+        </div>
+    )
+}
+
+export default Home;
