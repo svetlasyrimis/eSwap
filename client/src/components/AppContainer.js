06d852a548326f05a1189b534f7659480f02432f
Dwayne <dmoneymunro@gmail.com>
2020-03-12 11:10:15 -0400

    added react router and sign up component

--- client/src/components/AppContainer.js
+++ client/src/components/AppContainer.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import Home from './Home';
 import { Route, Switch } from 'react-router-dom';
+import SignUp from './SignUp';
 // import { Route } from 'react-router-dom';
 
 export class AppContainer extends Component {
@@ -9,6 +10,7 @@ export class AppContainer extends Component {
             <div>
                 <Switch>
                     <Route exact path="/" component={Home} />
+                    <Route path="/sign-up" component={SignUp} />
                 </Switch>
             </div>
         )
--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,15 +1,18 @@
 import React from 'react';
 import { Jumbotron, Button } from 'react-bootstrap';
+import { NavLink } from 'react-router-dom';
 
 const Home = () => {
     return (
         <div className="homepage">
             <div className="top-img">
-                <img src="/img/top-banner.jpg" alt="" width="100%" height="400px" />
+                {/* <img src="/img/top-banner.jpg" alt="" width="100%" height="400px" /> */}
             </div>
             <Jumbotron className="home-container">
                 <div className="home-nav" style={{ display: "flex", justifyContent: "space-between" }}>
-                    <span>Sign Up</span>
+                    <span>
+                        <NavLink to="/sign-up">Sign Up</NavLink>
+                    </span>
                     <span>Login</span>
                 </div>
                 <h1 className="home-name">Mando List</h1>
@@ -17,7 +20,7 @@ const Home = () => {
                 <Button>Search</Button>
             </Jumbotron>
             <div className="bottom-img">
-                <img src="img/bottom-banner.jpg" alt="" width="100%" height="400px" />
+                {/* <img src="img/bottom-banner.jpg" alt="" width="100%" height="400px" /> */}
             </div>
         </div>
     )
