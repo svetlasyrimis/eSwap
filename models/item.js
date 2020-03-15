bc6682b634890b557b675467c12682feca466ea9
Artemie Amari <yaroamari@gmail.com>
2020-03-14 20:25:53 -0400

    nada

--- models/item.js
+++ models/item.js
@@ -1,14 +1,12 @@
 const mongoose = require('mongoose')
 const Schema = mongoose.Schema
-
 const Item = new Schema(
   {
-//    title: { type: String, required: true },
-//    link: { type: String, required: true },
-		username: {type: String, required: true}
+    name: { type: String, required: true },
+    description: { type: String, required: true},
+    link: { type: String, required: true },
     user_id:{type: Schema.Types.ObjectId, ref: 'user_id'}
   },
   { timestamps: true }
 )
-
 module.exports = mongoose.model('items', Item)
--- seed/items.js
+++ seed/items.js
@@ -2,24 +2,34 @@ const db = require('../db')
 const Item = require('../models/item')
 const User = require('../models/user')
 db.on('error', console.error.bind(console, 'MongoDB connection error:'))
+const faker = require('faker')
 const main = async () => {
-///	try{
-    //const user1 = await User.find({ username: 'Lia'})
-	  //const user2 = await User.find({username:'Pink'})
-    //console.log('user1', user1)
-	  //console.log('user2', user2)
-    //const items = ([
-		//	{ username: 'Lia', user_id: user1[0]._id}
-		//],
-   // await db.Item.insertMany(items())
-    //console.log("Created items!")
-	//	)} catch(error){
-//	console.log(error)
-	}
+    const user1 = await User.find({ username: 'Joe'})
+    const user2 = await User.find({ username: 'Schmo'})
+//this only uses user1...testing
+    // const items = [...Array(100)].map(item => (
+    const items = [
+        {
+            name: faker.lorem.word(),
+            description: faker.lorem.sentence(),
+            link: faker.lorem.sentence(),
+            user_id: user1._id
+        },
+        {
+            name: faker.lorem.word(),
+            description: faker.lorem.sentence(),
+            link: faker.lorem.sentence(),
+            user_id: user2._id
+        }
+      ]
+
+    await Item.insertMany(items)
+    console.log("Created items!")
 }
 
 const run = async () => {
     await main()
     db.close()
 }
+
 run()
